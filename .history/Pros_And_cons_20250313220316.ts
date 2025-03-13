b94a8ddec7c6428a877373ad16bab80a9cd98965
ankitgujre <kumarankit65494@gmail.com>
2025-03-13 22:07:04 +0530

    update

---
+++ .history/Pros_And_cons_20250313220316.ts
@@ -0,0 +1,4 @@
+/**
+ * =======================================Static typing =================================>
+ * 1. Why? Static typing allows you to catch errors at compile time rather than runtime. This means that you can catch errors before they happen, which can save you time and money.
+ */
